2851439eb05553e98a473df0f1f5eccf3813f712
anshika-thakur <anshikasain005@gmail.com>
2024-09-17 20:40:43 +0530

    component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,5 @@
 import Chai from "./chai"
-
+// Component : it is a piece of ui that has its own logic and appreance.a component can be small as button and large as web page
 function App() {
   const username="chai aur code"
 
